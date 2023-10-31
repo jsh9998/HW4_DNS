b7042dfb41b5377f65e3c7ed9a96cdbbd31b7db3
Jonathan Henry <jsh9998@nyu.edu>
2023-10-30 23:09:05 -0500

    test
    test

--- DNSClient.py
+++ DNSClient.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     # Call the function to compare the results from both DNS servers and print the result
     
     result = compare_dns_servers(domainList,question_type)
-    #result = query_local_dns_server('nyu.edu.',question_type)
+    result = query_local_dns_server('nyu.edu.',question_type)
     print(result)
     
     #print(exfiltrate_info())
